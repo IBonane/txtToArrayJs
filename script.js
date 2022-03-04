077687d92a134e4c59d4422a7391277a6018ee20
Ibonane <bonanedjimbala@gmail.com>
2022-03-04 17:41:08 +0100

    first

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var textarea = document.getElementById('list');
-var arr = [];
-textarea.addEventListener('input', function () {
-    arr = this.value.split('\n');
-    console.log(arr);
-}, false);
+// var textarea = document.getElementById('list');
+// var arr = [];
+// textarea.addEventListener('input', function () {
+//     arr = this.value.split('\n');
+//     console.log(arr);
+// }, false);
 
 
